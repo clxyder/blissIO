6be9d5116ba75f468d679dc862f9923e2a1a201d
Carlos A. Wong <cwong@fsu.edu>
2020-05-18 12:18:32 -0400

    added text input to action button

--- components/ActionButton.js
+++ components/ActionButton.js
@@ -4,6 +4,7 @@ import {
     Text,
     View,
     Dimensions,
+    TextInput,
     TouchableOpacity,
 } from 'react-native';
 import {Ionicons} from '@expo/vector-icons';
@@ -11,16 +12,21 @@ import {Ionicons} from '@expo/vector-icons';
 const WIDTH = Dimensions.get('window').width;
 
 export const ActionButton = function(props) {
+    const cb = props.cb ? props.cb : () => console.log('No callback function!')
+    const [value, onChangeText] = React.useState('How will you change your world?');
     return (
-        <TouchableOpacity onPress={() => {}} style={styles.container}>
+        <TouchableOpacity onPress={() => {cb()}} style={styles.container}>
             <View style={styles.leftCol}>
                 <Text style={{fontSize: 9}}>{'\u25A0'}</Text>
             </View>
-            <View style={styles.centerCol}>
+            {/* <View style={styles.centerCol}>
                 <Text style={{fontSize: 18, color: '#545454'}}>
                     How will you change your world?
                 </Text>
-            </View>
+            </View> */}
+            <TextInput style={styles.centerCol}
+                onChangeText={text => onChangeText(text)}
+                value={value}/>
             <View style={styles.rightCol}>
                 <Ionicons name='md-globe' color = '#000000' size={24} style={{alignSelf: 'center'}}/>
             </View>
@@ -41,9 +47,9 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         shadowColor: '#000000',
         alignItems: 'center',
-        elevation: 6,
-        shadowRadius: 3,
-        shadowOpacity: 1.0,
+        elevation: 3,
+        shadowRadius: 1.5,
+        shadowOpacity: 0.3,
     },
     leftCol: {
         flex: 1,
@@ -51,6 +57,10 @@ const styles = StyleSheet.create({
     },
     centerCol:{
         flex: 4,
+        fontSize: 14.4,
+        // height: 40, 
+        // borderColor: 'gray', 
+        // borderWidth: 1,
     },
     rightCol: {
         flex: 1,
